31a57f700fe6dc4927f66276c27cd3de4c8633df
ST12-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 14:00:01 +0100

    Dodaj pliki projektów.

---
+++ Pętla For/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Obliczanie wartości funkcji y = root(3^2) * x");
+
+        for (int x = 0; x <= 10; x++)
+        {
+            double y = Math.Sqrt(Math.Pow(3, 2)) * x;
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+        }
+
+        Console.ReadLine();
+    }
+}
